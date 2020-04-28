a577886c2414477a58c6b3bb890ed2316811fc81
mouachan <mourad.ouachani@gmail.com>
2020-04-28 22:22:50 +0200

    build java container v2

--- manifest/build.sh
+++ manifest/build.sh
@@ -1,10 +1,15 @@
 
 #build and push to quay java image v1
 ./mvnw clean package  -Dquarkus.container-image.build=true -Dquarkus.container-image.name=frequent-flyer -Dquarkus.container-image.tag=java-1.0  -Dquarkus.native.container-build=true 
-docker run -i --rm -p 8080:8080 mouachani/frequent-flyer:java-1.0 
-docker commit 20971b982db6 quay.io/mouachan/frequent-flyer:java-1.0
+docker tag mouachani/frequent-flyer:java-1.0 quay.io/mouachan/frequent-flyer:java-1.0
 docker push quay.io/mouachan/frequent-flyer:java-1.0
 oc apply -f manifest/frequent-flyer-service-java-v1.yml 
+
+#build and push to quay java image v2
+./mvnw clean package  -Dquarkus.container-image.build=true -Dquarkus.container-image.name=frequent-flyer -Dquarkus.container-image.tag=java-2.0  -Dquarkus.native.container-build=true 
+docker tag mouachani/frequent-flyer:java-2.0 quay.io/mouachan/frequent-flyer:java-2.0
+docker push quay.io/mouachan/frequent-flyer:java-2.0
+oc apply -f manifest/frequent-flyer-service-java-v2.yml 
 #build and push to quay native image v1
 ./mvnw clean package  -Dquarkus.container-image.build=true -Dquarkus.container-image.name=frequent-flyer -Dquarkus.container-image.tag=native-1.0 -Pnative  -Dquarkus.native.container-build=true 
 docker run -i --rm -p 8080:8080 mouachani/frequent-flyer:native-1.0 
